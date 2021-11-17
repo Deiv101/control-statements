2139e6d9115e46596039ed4351d4a62cadcaf682
Deiv101 <hrakojoana@yahoo.com>
2021-11-17 13:32:48 +0200

    while and if in one

--- control_statements.py
+++ control_statements.py
@@ -53,3 +53,24 @@ for grade in grades:
 # termination phase
 average = total / grade_counter
 print(f'Class average is {average}')
+
+#___________________________________________________________________________________________________________________________________________________________________
+
+# initialization phase
+total = 0 # sum of grades
+grade_counter = 0
+
+# processing phase
+grade = int(input('Enter grade, -1 to end: ')) # get one grade
+
+while grade != -1:
+    total += grade 
+    grade_counter += 1 
+    grade = int(input('Enter grade, -1 to end: '))
+
+# termination phase
+if grade_counter != 0:
+    average = total / grade_counter
+    print(f'Class average is {average: .2f}')
+else:
+    print('No grades were entered')
