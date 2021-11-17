29737a7c2f5f1f896ff069aaf64f2ba87d0b46f1
Deiv101 <hrakojoana@yahoo.com>
2021-11-17 12:17:20 +0200

    while & for statements

--- control_statements.py
+++ control_statements.py
@@ -5,3 +5,14 @@ product = 3
 while product <= 50:
     product *= 3
     print(product)
+
+# for statement
+for character in 'Programming':
+    print(character, end=' ')
+
+#_____________________________________________________________________________________________________________________________________________________________________________________________
+total = 0
+list = [2, -3, 0, 17, 9]
+for number in list:
+    total = total + number 
+    print(total, '\t', end='')
