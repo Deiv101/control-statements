e39051fd5f1a46c6a4ee008d33046754952d25a9
Deiv101 <hrakojoana@yahoo.com>
2021-11-17 18:37:36 +0200

    compound interest calculation

--- control_statements.py
+++ control_statements.py
@@ -81,3 +81,18 @@ amount = 132.54
 print(f'{amount:.5f}')
 
 #___________________________________________________________________________________________________________________________________________________________________
+# Compund-Interest Calculation
+"""We will compute compound interest using the Decimal type for precise monetary calculation."""
+# Problem Statement
+"""A person invests $1000 in a savings account yielding 5% interest. Assuming that
+the person leaves all interest on deposit in the account, calculate and display the
+amount of money in the account at the end of each year for 10 years"""
+
+"""Use the following formula for determining these amounts:
+a = p(1 + r)^n
+where
+p is the original amount invested (i.e., the principal),
+r is the annual interest rate,
+n is the number of years and
+a is the amount on deposit at the end of the nth year
+"""
