d93c9b313073b0cb68e404d0e7c94fd441f28821
Deiv101 <hrakojoana@yahoo.com>
2021-11-21 08:39:05 +0200

    6,000,000 Rolls

--- control_statements.py
+++ control_statements.py
@@ -126,4 +126,44 @@ for n in range(10):
 import random
 
 for roll in range(10):
-    print(random.randrange(1, 7), end= ' ')
+    print(random.randrange(1, 7),'\n')
+
+#___________________________________________________________________________________________________________________________________________________________________
+# 6,000,000 Rolls
+# fig04_01.py
+"""Roll a six-sided die 6,000,000 times."""
+import random
+
+# face frequency counters
+freq1 = 0
+freq2 = 0
+freq3 = 0
+freq4 = 0
+freq5 = 0
+freq6 = 0
+
+# 6,000,000 die rolls
+for roll in range(6_000_000): # note inderscore separators
+    face = random.randrange(1, 7)
+
+    # increment appropriate face counter
+    if face == 1:
+        freq1 += 1
+    elif face == 2:
+        freq2 += 1
+    elif face == 3:
+        freq3 += 1
+    elif face == 4:
+        freq4 += 1
+    elif face == 5:
+        freq5 += 1
+    elif face == 6:
+        freq6 += 1
+
+print(f'Face{"Frequency":>13}')
+print(f'{1:>4}{freq1:>13}')
+print(f'{2:>4}{freq2:>13}')
+print(f'{3:>4}{freq3:>13}')
+print(f'{4:>4}{freq4:>13}')
+print(f'{5:>4}{freq5:>13}')
+print(f'{6:>4}{freq6:>13}')
