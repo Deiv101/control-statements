560589479641d27cafb8c8884a8a28b43ab23e8a
Deiv101 <hrakojoana@yahoo.com>
2021-11-22 02:26:00 +0200

    second function: display_dice

--- control_statements.py
+++ control_statements.py
@@ -193,3 +193,8 @@ def roll_dice():
     die2 = random.randrange(1, 7)
     return (die1, die2) # pack die face values into a tuple
 
+def display_dice(dice):
+    """Display one roll of the dice."""
+    die1, die2 = dice # unpack the tuple into variables die1 and die2
+    print(f'Player rolled {die1} +{die2} = {sum(dice)}')
+
